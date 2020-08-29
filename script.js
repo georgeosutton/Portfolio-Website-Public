0ee5153fc3fabc3e33d0a5734f984fc62bb56d2c
georgeosutton <46813782+georgeosutton@users.noreply.github.com>
2020-08-29 15:30:10 +0100

    Update

--- script.js
+++ script.js
@@ -1,77 +1,61 @@
 var mqMobile = window.matchMedia("(max-width: 750px)");
-var mqTablet = window.matchMedia("(max-width: 1200px");    
-
-
+var mqTablet = window.matchMedia("(max-width: 1200px");
 
 var projects = [];
 
+function createProjects() {
+  for (let i = 0; i < 2; i++) {
+    projects.push({
+      project: document.createElement("div"),
+      indicator: document.createElement("div"),
+    });
+  }
 
-function createProjects(){
-   
-    for (let i = 0; i < 2; i++){
-        projects.push(
-            {
-                project: document.createElement("div"),
-                indicator: document.createElement("div")
-                
-            })
-    }
+  for (let i = 0; i < projects.length; i++) {
+    projects[i].project.className = "projects";
+    projects[i].indicator.className = "indicator";
+    console.log(projects[i].indicator);
+    document.getElementById("indicators").appendChild(projects[i].indicator);
+  }
 
-    for (let i = 0; i < projects.length; i++){
-        projects[i].project.className = 'projects'
-        projects[i].indicator.className = 'indicator'
-        console.log(projects[i].indicator)
-        document.getElementById('indicators').appendChild(projects[i].indicator);
-   }
-
-   
-
-    projects[0].project.innerHTML 
-    = "<div class=\"project\"><img class=\"thumbnail\" src=\"imgs/project-personal-portfolio.png\" alt=\"\"><div class=\"project-preview\">" +
-       "<h6 class=\"project-title\">Personal Portfolio Site</h6><p>View the code behind this site; built using HTML, CSS and JavaScript.</p>" +
-       "<div class=\"icon-wrap\"> <a href=\"https://github.com/georgeosutton/Portfolio-Website-Public\">GitHub</a>" +
-       "<a href=\"https://github.com/georgeosutton/Portfolio-Website-Public\"> <img class=\"github-icon\"" + 
-       " src=\"imgs/GitHub-Mark-32px.png\" alt=\"GitHub Link\"></a></div></div></div>";
-    projects[1].project.innerHTML 
-    = "<div class=\"project\"><img class=\"thumbnail\" src=\"imgs/coming-soon.jpg\" alt=\"\"><div class=\"project-preview\">" +
-      "<h6 class=\"project-title\">WordPress E-commerce Site </h6><p>Work in progress</p></div></div>";
-   
-    
+  projects[0].project.innerHTML =
+    '<div class="project"><img class="thumbnail" src="imgs/project-personal-portfolio.png" alt=""><div class="project-preview">' +
+    '<h6 class="project-title">Personal Portfolio Site</h6><p>View the code behind this site; built using HTML, CSS and JavaScript.</p>' +
+    '<div class="icon-wrap"> <a href="https://github.com/georgeosutton/Portfolio-Website-Public">GitHub</a>' +
+    '<a href="https://github.com/georgeosutton/Portfolio-Website-Public"> <img class="github-icon"' +
+    ' src="imgs/GitHub-Mark-32px.png" alt="GitHub Link"></a></div></div></div>';
+  projects[1].project.innerHTML =
+    '<div class="project"><img class="thumbnail" src="imgs/coming-soon.jpg" alt=""><div class="project-preview">' +
+    '<h6 class="project-title">Coming Soon! </h6><p>Work in progress</p></div></div>';
 }
-
 createProjects();
 projectsView();
 
-    function projectsView(){
-        var displayed = document.querySelectorAll('.projects')
-        displayed.forEach(function(displayed){
-            displayed.remove()
-   }
-   )
-    
-   document.getElementById('project-wrapper').appendChild(projects[0].project);
-   projects[0].indicator.className = 'active indicator';
-   
+function projectsView() {
+  var displayed = document.querySelectorAll(".projects");
+  displayed.forEach(function (displayed) {
+    displayed.remove();
+  });
 
+  document.getElementById("project-wrapper").appendChild(projects[0].project);
+  projects[0].indicator.className = "active indicator";
 }
 
 mqMobile.addListener(projectsView);
 mqTablet.addListener(projectsView);
 
-var before = document.getElementById('before');
-before.addEventListener('click', function(){
-    projects[0].indicator.className = 'indicator';
-    let first = projects.shift();
-        projects.push(first);
-        projectsView()
-    });
+var before = document.getElementById("before");
+before.addEventListener("click", function () {
+  projects[0].indicator.className = "indicator";
+  let first = projects.shift();
+  projects.push(first);
+  projectsView();
+});
 
-var next = document.getElementById('next');
-next.addEventListener('click', function(){
-    projects[0].indicator.className = 'indicator';
-    let last = projects.pop();
-        projects.unshift(last); 
-        projectsView()
-    });
-
-
+var next = document.getElementById("next");
+next.addEventListener("click", function () {
+  projects[0].indicator.className = "indicator";
+  let last = projects.pop();
+  projects.unshift(last);
+  projectsView();
+});
